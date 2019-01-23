f953395a872b7bbde450ef95b85308b948eebd2f
Chip Wickings <34577860+Chip-W@users.noreply.github.com>
2019-01-23 16:40:55 -0500

    initial commit

---
+++ locales/en.lua
@@ -0,0 +1,99 @@
+--[[
+ORIGINAL CODE BY ESX-ORG / ESX_POLICEJOB
+EDITS BY CHIP W
+]]
+
+Locales['en'] = {
+  -- Cloakroom
+  ['cloakroom']                     = 'locker room',
+  ['citizen_wear']                  = 'civilian Outfit',
+  ['laborer_wear']                  = 'Laborer PPE',
+  ['operator_wear']                 = 'Operator PPE',
+  ['foreman_wear']                  = 'Foreman PPE',
+  ['boss_wear']                     = 'Contractor PPE',
+  ['no_outfit']                     = 'there\'s no outfit in the locker for you!',
+  ['open_cloackroom']               = 'press ~INPUT_CONTEXT~ to put on ~y~PPE~s~.',
+
+  -- Service
+  ['service_max']                   = 'take the day off, we already have enough guys on the job. : %s/%s',
+  ['service_not']                   = 'you have not ready for work! You\'ll have to put on your PPE first.',
+  ['service_anonunce']              = 'service information',
+  ['service_in']                    = 'welcome! time to get some work done',
+  ['service_in_announce']           = 'worker ~y~%s~s~ has came to work!',
+  ['service_out']                   = 'you are done for the day. time to hit the Yellow Jack',
+  ['service_out_announce']          = 'worker ~y~%s~s~ has left work.',
+
+
+  -- Vehicles
+  ['vehicle_menu']                  = 'vehicle',
+  ['vehicle_blocked']               = 'all available spawn points are currently blocked!',
+  ['garage_prompt']                 = 'press ~INPUT_CONTEXT~ to access the ~y~Vehicle Actions~s~.',
+  ['garage_title']                  = 'git\'r Done Vehicle Menu',
+  ['garage_stored']                 = 'stored',
+  ['garage_notstored']              = 'not in garage',
+  ['garage_storing']                = 'we\'re attempting to remove the vehicle, make sure no players are around it.',
+  ['garage_has_stored']             = 'the vehicle has been stored in your garage',
+  ['garage_has_notstored']          = 'no nearby owned vehicles were found',
+  ['garage_notavailable']           = 'your vehicle is not stored in the garage.',
+  ['garage_blocked']                = 'there\'s no available spawn points!',
+  ['garage_empty']                  = 'you dont have any vehicles in your garage.',
+  ['garage_released']               = 'your vehicle has been released from the garage.',
+  ['garage_store_nearby']           = 'there is no nearby vehicles.',
+  ['garage_storeditem']             = 'open garage',
+  ['garage_storeitem']              = 'store vehicle in garage',
+  ['garage_buyitem']                = 'vehicle shop',
+  ['shop_item']                     = '$%s',
+  ['vehicleshop_title']             = 'vehicle Shop',
+  ['vehicleshop_confirm']           = 'do you want to buy this vehicle?',
+  ['vehicleshop_bought']            = 'you have bought ~y~%s~s~ for ~r~$%s~s~',
+  ['vehicleshop_money']             = 'you cannot afford that vehicle',
+  ['vehicleshop_awaiting_model']    = 'the vehicle is currently ~g~DOWNLOADING & LOADING~s~ please wait',
+  ['confirm_no']                    = 'no',
+  ['confirm_yes']                   = 'yes',
+
+  -- Prop Menu
+  ['object_spawner']                = 'Construction Site Items',
+
+  -- Traffic interaction
+  ['traffic_interaction']           = 'Construction Items Menu',
+  ['cone']                          = 'standard cone',
+  ['cone_2']                        = 'tall cone',
+  ['block_pallet_1']                = 'pallet of blocks',
+  ['road_work_ahead_barrier']       = 'road work ahead barrier',
+  ['large_barrier']                 = 'standard large barrier',
+  ['cement_mixer']                  = 'portable cement mixer',
+  ['pallet_cement_bags']            = 'ready mix cement bags',
+  ['load_pipe']                     = 'load of pipe',
+  ['wheel_barrow']                  = 'wheel barrow',
+  ['wood_pile_1']                   = 'small stack of wood planks',
+  ['wood_pile_2']                   = 'stack of wood 4x6\'s',
+  ['wood_pile_3']                   = 'large stack of wood planks',
+  ['work_light']                    = 'portable work light',
+  ['barrel_barrier']                = 'standard road construction barrier',
+  ['small_barrier_1']               = 'small sidewalk barrier',
+  ['small_barrier_2']               = 'small sidewalk barrier with light',
+  ['uneven_pavement_sign']          = 'uneven pavement sign',
+  ['medium_barrier']                = 'medium sawhorse barrier',
+  ['medium_barrier_sign']           = 'medium sawhorse barrier with sign',
+
+  -- Boss Menu
+  ['open_bossmenu']                 = 'press ~INPUT_CONTEXT~ to open the menu',
+  ['quantity_invalid']              = 'invalid quantity',
+  ['have_withdrawn']                = 'you have withdrawn ~y~%sx~s~ ~b~%s~s~',
+  ['have_deposited']                = 'you have deposited ~y~%sx~s~ ~b~%s~s~',
+  ['quantity']                      = 'quantity',
+  ['inventory']                     = 'inventory',
+  ['police_stock']                  = 'police Stock',
+
+  --Invoices
+  ['billing']                       = 'Create Invoice',
+  ['invoice_amount']               = 'amount To Charge Customer',
+
+  -- Misc
+  ['remove_prop']                   = 'press ~INPUT_CONTEXT~ to delete the object',
+  ['map_blip']                      = 'Git\'r Done Construction',
+
+  -- Notifications
+  ['alert_gitrdone']                = 'message GrD Construction',
+  ['phone_gitrdone']                = 'GrD Construction'
+}
